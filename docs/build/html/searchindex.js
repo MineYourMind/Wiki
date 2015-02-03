e2b4891a77da701b219a59a925e70cb0eb76b611
none <mineyourmindger@gmail.com>
2015-02-03 13:25:57 +0100

    initial

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({titleterms:{indic:0,tabl:0,mymwiki:0,welcom:0,document:0},filenames:["index"],objects:{},envversion:43,terms:{content:0,index:0,search:0,page:0,modul:0},objnames:{},objtypes:{},titles:["Welcome to MyMWiki&#8217;s documentation!"]})
